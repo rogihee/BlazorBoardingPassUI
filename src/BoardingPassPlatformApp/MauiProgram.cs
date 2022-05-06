2473038c7d291d9c73e3502de2294abfe4a75731
Rogier van der Hee <rogier@ix-tech.nl>
2022-05-06 15:53:49 +0200

    Attempt at setting the status bar background color

--- src/BoardingPassPlatformApp/MauiProgram.cs
+++ src/BoardingPassPlatformApp/MauiProgram.cs
@@ -8,6 +8,9 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
+    	// change color:
+        // https://material.io/resources/color/#!/?view.left=0&view.right=1
+
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
